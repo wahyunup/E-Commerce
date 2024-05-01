05cb0bd1eb306f342cb8b4eb890ff6e6805893a2
wahyunup <wahyunurpambuko@gmail.com>
2024-05-01 16:45:44 +0700

    add change

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Footer from './components/footer/Footer.jsx'
 function App() {
 
   return (
-    <div>
+    <div className='flex flex-col justify-between h-screen'>
       <div className="sticky top-0 z-10">
     <Navbar/>
       </div>
     {/* Product */}
-    <section id='productList' className='flex gap-5 justify-center flex-wrap px-10 mt-40'>
+    <section id='productList' className='px-10 py-10 flex items-center justify-center'>
+      <div  className='flex gap-5 flex-wrap justify-center'>
       <Card 
       imageList='https://images.unsplash.com/photo-1596455607563-ad6193f76b17?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       name='Baseball cap'
@@ -22,20 +23,21 @@ function App() {
       summaryText=' The perfect blend of retro and contemporary style, combining the spirit of classic hip-hop with a modern feel. With an awesome design, this t-shirt is a fashion statement that stands out with iconic elements from the legendary group Run DMC and the iconic brand Adidas.'
       />
       <Card 
-      imageList='https://images.unsplash.com/photo-1596455607563-ad6193f76b17?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-      name='Baseball cap'
-      summaryText='Display your sporty style with radiating confidence with the Ultimate FlexFit Baseball Cap.'
+      imageList='https://images.unsplash.com/photo-1623998021446-45cd9b269056?q=80&w=2079&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      name='Expensive watches'
+      summaryText='The "Timeless Elegance" watch is a perfect blend of aesthetic beauty and unparalleled functionality. Designed for those who appreciate classic style but still want to look modern, this watch is a manifestation of timeless elegance.'
       />
       <Card 
-      imageList='https://images.unsplash.com/photo-1596455607563-ad6193f76b17?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-      name='Baseball cap'
-      summaryText='Display your sporty style with radiating confidence with the Ultimate FlexFit Baseball Cap.'
+      imageList='https://images.unsplash.com/photo-1624378441864-6eda7eac51cb?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      name='Skena jeans'
+      summaryText='Skena Performance "Skena Jeans" pants are the ultimate choice for those who prioritize performance and style in every step. Designed with the latest technology and high-quality materials, these pants provide the perfect combination of comfort, practicality and unparalleled style.'
       />
       <Card 
-      imageList='https://images.unsplash.com/photo-1596455607563-ad6193f76b17?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-      name='Baseball cap'
-      summaryText='Display your sporty style with radiating confidence with the Ultimate FlexFit Baseball Cap.'
+      imageList='https://images.unsplash.com/photo-1551028719-00167b16eac5?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      name='leather jacket'
+      summaryText='The "Heritage Craft" leather jacket is a manifestation of timeless traditional beauty and superb modern craftsmanship. Meticulously crafted by skilled artisans, this jacket is not only an elegant style choice but also a legacy that will last forever.'
       />   
+      </div>
     </section>
     {/* footer */}
     <Footer/>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,61 +1,34 @@
+import { useState } from "react";
+
 function Navbar () {
+  const [toogleNavbar, setToogleNavbar] = useState(false);
     return(
+
         <div>
-<header class="bg-white sticky w-full bg-white z-100 border-b border-slate-100">
-  <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 ">
-    <div class="flex h-16 items-center justify-between">
-      <div class="md:flex md:items-center md:gap-12">
+<header className="bg-white w-full bg-white border-b border-slate-100">
+  <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 ">
+    <div className="flex h-16 items-center justify-between">
+      <div className="md:flex md:items-center md:gap-12">
         <a href="#">
-          <span class="text-pink-500 font-medium">Shopingin</span>
+          <span className="text-pink-500 font-medium">Shopingin</span>
         </a>
       </div>
 
-      <div class="hidden md:block">
-        <nav aria-label="Global">
-          <ul class="flex items-center gap-6 text-sm">
-            <li>
-              <a class="text-gray-500 transition hover:text-gray-500/75" href="#"> Assecoris </a>
-            </li>
 
-            <li>
-              <a class="text-gray-500 transition hover:text-gray-500/75" href="#"> Dress </a>
-            </li>
+      <div className="flex items-center gap-4">
+        <div className="sm:flex sm:gap-4">
 
-            <li>
-              <a class="text-gray-500 transition hover:text-gray-500/75" href="#"> Pants </a>
-            </li>
-
-          </ul>
-        </nav>
-      </div>
-
-      <div class="flex items-center gap-4">
-        <div class="sm:flex sm:gap-4">
-
-          <div class="hidden sm:flex ">
+          <div>
             <a
-              class="  text-sm font-medium text-gray-500 py-2 px-3 bg-slate-50 rounded-3xl"
+              className="  text-sm font-medium text-gray-500 py-2 px-3 bg-slate-50 rounded-3xl"
               href="#"
             >
-             <i class="fa-solid fa-bag-shopping"></i>
+             <i className="fa-solid fa-bag-shopping"></i>
             </a>
           </div>
         </div>
 
-        <div class="block md:hidden">
-          <button class="rounded p-2 text-gray-600 transition hover:text-gray-600/75">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
-        </div>
+        
       </div>
     </div>
   </div>
